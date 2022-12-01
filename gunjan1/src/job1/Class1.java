7528a07acd62fc9fabdfae91273b99c2ac69e03d
abc <abc@ASHWINI-PC>
2022-12-01 13:14:47 +0530

    [Ashwini]: Some Basic programmes

---
+++ gunjan1/src/job1/Class1.java
@@ -0,0 +1,22 @@
+package job1;
+
+public class Class1 
+{
+	public static void main (String[]args)
+	
+{
+     String personname ;
+     String livingplace;
+     
+  
+
+	personname = "ashu";
+	livingplace = "khamgaon";
+	
+   System.out.println(personname);
+   System.out.println(livingplace);
+   
+	
+}
+
+}
---
+++ gunjan1/src/method1/sample2.java
@@ -0,0 +1,18 @@
+package method1;
+
+public class sample2
+{
+// 2.Static Regular method call from different class
+	public static void main (String[]args)
+	{
+		Sample4.Personname(); // classname.methodname() or //Diff classname.methodname()
+		variable3.PersonDept();
+		variable3.Personjob();
+		
+		
+		System.out.println("Personname");
+		System.out.println("PersonDept");
+		System.out.println("Personjob");
+		
+	}
+}
